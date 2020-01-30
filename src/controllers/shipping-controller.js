d29c124856f25a744b99b99425fd55dc0617d51a
Steffen Korth <60397312+SteffenKorth@users.noreply.github.com>
2020-01-30 12:07:46 +0100

    Update shipping-controller.js

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -4,8 +4,8 @@ var productService = require('../services/product-service')
 class ShippingController {
 
   constructor() {
-    this.REGULAR_PRICE = 0.1
-    this.OVERNIGHT_PRICE = 1
+    this.REGULAR_PRICE = 1
+    this.OVERNIGHT_PRICE = 10
   }
 
   async getItemShipping(item) {
